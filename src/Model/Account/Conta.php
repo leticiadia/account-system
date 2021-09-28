04bcdb1a62983f6a83ec9f93600aa7bdd1dec521
Leticia Dias <contatoleticiadia@gmail.com>
2021-09-28 15:55:57 -0300

    Feat: adding the Model and Account folders to project structure

---
+++ src/Model/Account/Conta.php
@@ -0,0 +1,72 @@
+<?php
+
+class Conta
+{
+    private $titular;
+    private $saldo;
+    private static $numeroDeContas = 0;
+
+    public function __construct(Titular $titular)
+    {
+        $this->titular = $titular;
+        $this->saldo = 0;
+
+        self::$numeroDeContas++;
+    }
+
+    public function __destruct()
+    {
+        self::$numeroDeContas--;
+    }
+
+    public function sacar(float $valorASacar): void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
+
+    public function recuperarSaldo(): float
+    {
+        return $this->saldo;
+    }    
+
+    public function recuperarNomeTitular(): string
+    {
+       return $this->titular->recuperarNome();
+    }
+
+    public function recuperarCpfTitular(): string 
+    {
+        return $this->titular->recuperarCpfTitular();
+    }
+
+    public static function recuperarNumeroDeContas(): int
+    {
+        return self::$numeroDeContas;
+    }
+}
---
+++ src/Model/Endereco.php
@@ -0,0 +1,45 @@
+<?php
+
+    class Endereco 
+    {
+        private string $cidade;
+        private string $bairro;
+        private string $rua;
+        // Caso o número não precise fazer nenhum tipo de cálculo , não preciso colocar ele como int, apenas como string...
+        private string $numero;
+
+        public function __construct(string $cidade, string $bairro, string $rua, string $numero)
+        {
+            $this->cidade = $cidade;
+            $this->bairro = $bairro;
+            $this->rua = $rua;
+            $this->numero = $numero;
+        }
+
+
+        public function recuperarCidade(): string
+        {
+            return $this->cidade;
+        }
+
+        public function recuperarBairro(): string
+        {
+            return $this->bairro;
+        }
+
+        public function recuperarRua(): string 
+        {
+            return $this->rua;
+        }
+
+        public function recuperarNumero(): string 
+        {
+            return $this->numero;
+        }
+
+    }
+
+
+
+
+?>
