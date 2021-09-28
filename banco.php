2e3188aff57f449e10111c693adc3b7a4203c4d7
Leticia Dias <contatoleticiadia@gmail.com>
2021-09-28 17:15:19 -0300

    Feat: add namesapces with use

--- banco.php
+++ banco.php
@@ -1,22 +1,25 @@
 <?php
 
-require_once 'src/Conta.php';
-require_once 'src/Cpf.php';
-require_once 'src/Titular.php';
-require_once 'src/Endereco.php';
+    require_once 'autoload.php';
 
-$endereco = new Endereco('Rio de Janeiro', 'Fluminense', 'Minha rua', '78C');
-$primeiraConta = new Conta(new Titular(new Cpf('123.456.789-10'), 'Vinicius Dias',  $endereco));
-$primeiraConta->depositar(500);
-$primeiraConta->sacar(300); // isso é ok
+    use Project\Database\Model\Account\Titular;
+    use Project\Database\Model\Endereco;
+    use Project\Database\Model\Cpf;
+    use Project\Database\Model\Account\Conta;
 
-echo $primeiraConta->recuperarNomeTitular() . PHP_EOL;
-echo $primeiraConta->recuperarCpfTitular() . PHP_EOL;
-echo $primeiraConta->recuperarSaldo() . PHP_EOL;
+    $endereco = new Endereco('Rio de Janeiro', 'Fluminense', 'Minha rua', '78C');
+    $primeiraConta = new Conta(new Titular(new Cpf('123.456.789-10'), 'Vinicius Dias',  $endereco));
+    $primeiraConta->depositar(500);
+    $primeiraConta->sacar(300); 
 
-$segundaConta = new Titular(new Cpf('987.654.321-10'), 'Patricia',  $endereco);
-var_dump($segundaConta);
+    echo $primeiraConta->recuperarNomeTitular() . PHP_EOL;
+    echo $primeiraConta->recuperarCpfTitular() . PHP_EOL;
+    echo $primeiraConta->recuperarSaldo() . PHP_EOL;
 
-$outra = new Conta(new Titular(new Cpf('123.456.789-10'), 'Ana',  $endereco));
-unset($segundaConta);
-echo Conta::recuperarNumeroDeContas();
+    $segundaConta = new Titular(new Cpf('987.654.321-10'), 'Patricia',  $endereco);
+    var_dump($segundaConta);
+
+    $outra = new Conta(new Titular(new Cpf('123.456.789-10'), 'Ana',  $endereco));
+    unset($segundaConta);
+    echo Conta::recuperarNumeroDeContas();
+?>
