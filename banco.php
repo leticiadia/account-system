6e687738238af21013f40b62445c4f105f027477
Leticia Dias <contatoleticiadia@gmail.com>
2021-09-28 13:48:32 -0300

    Feat: importing Endereco file

--- banco.php
+++ banco.php
@@ -3,8 +3,10 @@
 require_once 'src/Conta.php';
 require_once 'src/Cpf.php';
 require_once 'src/Titular.php';
+require_once 'src/Endereco.php';
 
-$primeiraConta = new Conta(new Titular(new Cpf('123.456.789-10'), 'Vinicius Dias'));
+$endereco = new Endereco('Rio de Janeiro', 'Fluminense', 'Minha rua', '78C');
+$primeiraConta = new Conta(new Titular(new Cpf('123.456.789-10'), 'Vinicius Dias',  $endereco));
 $primeiraConta->depositar(500);
 $primeiraConta->sacar(300); // isso é ok
 
@@ -12,9 +14,9 @@ echo $primeiraConta->recuperarNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperarCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperarSaldo() . PHP_EOL;
 
-$segundaConta = new Titular(new Cpf('987.654.321-10'), 'Patricia');
+$segundaConta = new Titular(new Cpf('987.654.321-10'), 'Patricia',  $endereco);
 var_dump($segundaConta);
 
-$outra = new Conta(new Titular(new Cpf('123.456.789-10'), 'Ana'));
+$outra = new Conta(new Titular(new Cpf('123.456.789-10'), 'Ana',  $endereco));
 unset($segundaConta);
 echo Conta::recuperarNumeroDeContas();
