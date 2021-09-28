76277a0497dcb15f9e417e53567d9262e3d495cc
Leticia Dias <contatoleticiadia@gmail.com>
2021-09-28 15:06:25 -0300

    Feat: alterarNome() method created

---
+++ src/Funcionario.php
@@ -0,0 +1,25 @@
+<?php
+
+    class Funcionario extends Pessoa
+    {
+        private string $cargo;
+
+        public function __construct(string $nome, Cpf $cpf, string $cargo)
+        {
+            parent::__construct($nome, $cpf);
+            $this->cargo = $cargo;
+        }
+
+        public function recuperarCargo(): string
+        {
+            return $this->cargo;
+        }
+
+        public function alterarNome(string $nome): void
+        {
+            $this->validaNomeTitular($nome);
+            $this->nome = $nome;
+        }
+    }
+?>
+
