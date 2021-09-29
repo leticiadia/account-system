1565cbddba01551a0d4140df6bccce6098f5175d
Leticia Dias <contatoleticiadia@gmail.com>
2021-09-29 17:17:57 -0300

    Feat: add new EditorVide method

--- bonificacoes.php
+++ bonificacoes.php
@@ -3,12 +3,13 @@
     require_once 'autoload.php';
 
     use Project\Database\Service\ControladorDeBonificacoes;
-    use Project\Database\Model\Funcionario\{Desenvolvedor, Gerente, Diretor};
+    use Project\Database\Model\Functionary\{Desenvolvedor, Gerente, Diretor, EditorVideo};
     use Project\Database\Model\{Cpf};
 
-    $umFuncionario = new Desenvolvedor('Felipe Dias', new Cpf('123.456.789-10'), 'Desenvolvedor', 1000);
-    $umaFuncionaria = new Gerente('Aline Pereira', new Cpf('987.654.321-10'), 'Gerente', 1000);
-    $umDiretor = new Diretor('Ana Paula', new Cpf('321.465.798-10'), 'Diretor', 1000);
+    $umFuncionario = new Desenvolvedor('Felipe Dias', new Cpf('123.456.789-10'), 1000);
+    $umaFuncionaria = new Gerente('Aline Pereira', new Cpf('987.654.321-10'), 1000);
+    $umDiretor = new Diretor('Ana Paula', new Cpf('321.465.798-10'), 2600);
+    $umEditorVideo = new EditorVideo('Tiago Lemos', new Cpf('258.369.147-10'), 1500);
 
     $umFuncionario->sobeDeNivel();
 
@@ -16,6 +17,7 @@
     $controlador->adicionarBonificacao($umFuncionario);
     $controlador->adicionarBonificacao($umaFuncionaria);
     $controlador->adicionarBonificacao($umDiretor);
+    $controlador->adicionarBonificacao($umEditorVideo);
 
     echo $controlador->recuperarTotal();
 ?>
