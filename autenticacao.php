1ac52b49d89eb8c95babfaf4e2ea574cf5d561a3
Leticia Dias <contatoleticiadia@gmail.com>
2021-10-01 16:10:21 -0300

    Feat: news variables created

--- autenticacao.php
+++ autenticacao.php
@@ -3,12 +3,18 @@
     namespace Project\Database\Service;
 
     use Project\Database\Model\Functionary\Diretor;
-    use Project\Database\Model\{Cpf};
+    use Project\Database\Model\Functionary\Gerente;
+    use Project\Database\Model\Account\Titular;
+    use Project\Database\Model\{Cpf, Endereco};
 
     require_once 'autoload.php';
 
     $autenticador = new Autenticador();
-    $umDiretor = new Diretor('João Silva', new Cpf('159.456.357-10'), 100000);
+    $novoDiretor = new Diretor('João Silva', new Cpf('159.456.357-10'), 1000);
+    $novoGerente = new Gerente('Ana Cardoso', new Cpf('789.456.123-10'), 1500);
+    $novoTitular = new Titular(new Cpf('789.456.123-10'), 'Ana Cardoso', new Endereco('Teste cidade', 'Teste bairro', 'Teste rua', '1234'));
 
-    $autenticador->tentarLogin($umDiretor, '1234');
+     echo $autenticador->tentarLogin($novoDiretor, '4321') . PHP_EOL;
+     echo $autenticador->tentarLogin($novoGerente, '1234') . PHP_EOL;
+     echo $autenticador->tentarLogin($novoTitular, 'dcba') . PHP_EOL;
 ?>
